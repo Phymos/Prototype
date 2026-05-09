fb5ef3104bea61cd22f253d6dcb067ef5f43f0c3
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-05-10 00:52:27 +0300

    added sound class

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public string clipName;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    [Range(.1f, 3f)]
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource audioSource;
+}
