88c145e87ac880523caedbd14bdf5b25041004ed
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-05-13 01:34:56 +0300

    added some part of freeze effect

--- Assets/Scripts/FreezeEffect.cs
+++ Assets/Scripts/FreezeEffect.cs
@@ -1,6 +1,26 @@
-using UnityEngine;
-
-public class FreezeEffect
+public class FreezeEffect : StatusEffect
 {
-    
+    private float slowMultiplier;
+
+    public FreezeEffect(float duration, float slowMultiplier)
+    {
+        this.duration = duration;
+        this.remaining = duration;
+        this.elementType = ElementType.Ice;
+        this.slowMultiplier = slowMultiplier;
+    }
+
+     public override void OnApply(IDamageable target)
+    {
+        //hasar ver ve düşman hızını düşür
+        //vfxi başlat
+    }
+
+    public override void OnTick(IDamageable target, float deltaTime) { }
+
+    public override void OnExpire(IDamageable target)
+    {
+        // hızı geri ver
+        //vfxi kapa
+    }
 }
--- Assets/Scripts/StatusEffect.cs
+++ Assets/Scripts/StatusEffect.cs
@@ -1,6 +1,11 @@
-using UnityEngine;
 
-public class StatusEffect
+public abstract class StatusEffect
 {
-    
+    public ElementType elementType;
+    public float duration;
+    public float remaining;
+
+    public abstract void OnApply(IDamageable target);
+    public abstract void OnTick(IDamageable target, float deltaTime);
+    public abstract void OnExpire(IDamageable target);
 }
