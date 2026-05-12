fa4b37c2b7acc7f6a5b1f3eca9586b38d6a77b49
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-05-13 01:03:02 +0300

    added element datas

---
+++ Assets/Scripts/ElementSO.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ElementSO", menuName = "Scriptable Objects/ElementSO")]
+public class ElementSO : ScriptableObject
+{
+    [Header("Information")]
+    public string elementName;
+    public Sprite icon;
+    public Color color;
+
+    [Header("Damage")]
+    public float baseDamage;
+    public ElementType elementType;
+
+    [Header("Status Effect")]
+    public float statusChance;
+    public float statusDuration;
+
+    [Header("Visuals")]
+    public GameObject hitVFX;
+    public AudioClip hitSFX;
+}
---
+++ Assets/Scripts/ElementType.cs
@@ -0,0 +1,13 @@
+public enum ElementType
+{
+    Physical,
+    Fire,
+    Ice,
+    Lightning,
+    Poison,
+    Wind,
+    Earth,
+    Water,
+    Light,
+    Dark
+}
