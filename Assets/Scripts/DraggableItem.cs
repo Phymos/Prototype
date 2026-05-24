66473a061d93920dfeec24e64dd4e391d167ff72
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-05-24 03:29:30 +0300

    added inventory dragging

---
+++ Assets/Scripts/DraggableItem.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Transform parentAfterDrag;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parentAfterDrag);
+    }
+}
