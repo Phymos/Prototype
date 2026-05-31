808e82001ddfe6e9b38cc54942dec4e30f24cce1
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-06-01 02:01:39 +0300

    added hit flash

--- Assets/Scripts/Enemy/DummyEnemy.cs
+++ Assets/Scripts/Enemy/DummyEnemy.cs
@@ -1,8 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class DummyEnemy : MonoBehaviour, IDamageable
 {
     float health = 100f;
+    [SerializeField] Renderer[] renderers;
+    private MaterialPropertyBlock propBlock;
+
+    void Start()
+    {
+        propBlock = new MaterialPropertyBlock();
+    }
 
     void Update()
     {
@@ -15,6 +23,23 @@ public class DummyEnemy : MonoBehaviour, IDamageable
     public void TakeDamage(float amount)
     {
         health -= amount;
-        Debug.Log("damage taken" + health);
+        StartCoroutine(FlashHit());
+    }
+
+    IEnumerator FlashHit()
+    {
+        SetFlash(1f);
+        yield return new WaitForSeconds(0.1f);
+        SetFlash(0f);
+    }
+
+    void SetFlash(float amount)
+    {
+        foreach (var r in renderers)
+        {
+            r.GetPropertyBlock(propBlock);
+            propBlock.SetFloat("_HitAmount", amount);
+            r.SetPropertyBlock(propBlock);
+        }
     }
 }
